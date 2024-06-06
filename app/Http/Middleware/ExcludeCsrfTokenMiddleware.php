62808c0a8ea07a183ccda7a773845a1375fbbca3
ygyuri <gideonyuri15@gmail.com>
2024-06-06 12:39:45 +0300

    more corrections on csrf tokens

--- app/Http/Middleware/ExcludeCsrfTokenMiddleware.php
+++ app/Http/Middleware/ExcludeCsrfTokenMiddleware.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 class VerifyCsrfToken extends Middleware
 {
     protected $except = [
-        '/api/register',
-        '/api/login',
+        '*',
     ];
 }
